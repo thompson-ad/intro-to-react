5fc871327cdeee299b62b87c1e71160530da0182
Aaron Thompson <aaron.thompson@feedagency.com>
2019-11-14 17:09:49 +0000

    async, hooks and ajax

--- level-5/src/Pet.js
+++ level-5/src/Pet.js
@@ -1,35 +1,20 @@
-// We are going to rewrite the component from level 2 using JSX
-// JSX doesn't do much it just makes code a little more readable
-// if I write React.createElement("h1", { id: "main-title" }, "My Website");
-// I am trying to write <h1 id="main-title">My Website</h1>
-// JSX tries to shorcut this translation layer so you can just write what you mean
-// JSX is just translating those tags into React.createElement calls
-// because of this everywhere you have jsx you need to import react
-
 import React from "react";
 
-// export default function Pet({ name, animal, breed }) {
-//   return (
-//     <div>
-// if you remove the curly braces here it will give you 'name' on the screen
-// the curly braces mean that you are letting jsx know that it is a javascript expression so you could do like name.toUppercase() in the curly braces
-//       <h1>{name}</h1>
-//       <h2>{animal}</h2>
-//       <h2>{breed}</h2>
-//     </div>
-//   );
-// }
+export default function Pet({ name, animal, breed, media, location, id }) {
+  let hero = "http://placecorgi.com/300/300";
+  if (media.length) {
+    hero = media[0].small;
+  }
 
-const Pet = props => {
-  // note that you have to return just one thing as you would in a normal function
-  // so in this case you could not put an <h1></h1> above the div as that would be like returning 2 web components
   return (
-    <div className="">
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
-};
-
-export default Pet;
+}
--- level-5/src/SearchParams.js
+++ level-5/src/SearchParams.js
@@ -1,54 +1,77 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+// useEffect will help us with calling to the API
+// the API is from Petfinder.com and is US based
+// we install cross enc so that we get mock data from the API
+// when you run npm run dev now you will get mock data
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+import Results from "./Results";
 
-// this new component is like a search box so that we can search for little facets of pets
-// we will render this out to App.js
-// const SearchParams = () => {
-//   const location = "Seattle, WA";
-
-//   return (
-//     //   you can't use class because class is a reserved word in JS so you have to use className
-//     <div className="search-params">
-//       <form action="">
-//         <label htmlFor="location">
-//           location
-//           <input
-//             type="text"
-//             id="location"
-//             value={location}
-//             placeholder="location"
-//           />
-//         </label>
-//         <button>Submit</button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default SearchParams;
+// LIFECYCLE
+// SearchParams gets rendered for the first time
+// Sets all the hooks up
+// Location - Seattle WA
+// Breeds - []
+// Animal - "dog"
+// breed - ""
+// schedule the useEffect
+// return markup to the DOM so user will see it
+// run the sceduled effect
+// go out to the API with animal
+// get breeds back
+// call setBreeds
 
 const SearchParams = () => {
-  // location is the current state of location
-  // setLocation is an updater for that
-  // "Seattle, WA" is the default
-  // THIS IS A HOOK
-  //   useState is creating a Hook and when you get back a hook you get an array of 2 things
-  // 1. the current state of it
-  // 2. an updater function for that current piece of state
-  // All hooks begin with 'use'
-  //   so the below array is destructured from what useState always gives back
-  //   Hooks update the need to setState in functional components
-  //   you can think of hooks as something that gets 'hooked' or 'caught' every time the render function gets called
-  // DO NOT PUT HOOKS IN LOOPS OR IF STATEMENTS - they rely strongly on the order in which they are called
   const [location, setLocation] = useState("Seattle, WA");
-  const [animal, setAnimal] = useState("dog");
-  const [breed, updateBreed] = useState("");
-  const [breeds, updateBreeds] = useState([]);
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  // an async function is a function that is guranteed to return a promise
+  // await key word forces the function to wait until the pet.animals is complete
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+    // if you request something and nothinf comes back then set as empty array
+    setPets(animals || []);
+  }
+
+  // useEffect is going to replace several of the lifecycle hooks
+  // componentDidMount etc
+
+  // useEffect is disconnected from rendering and it is scheduling the function to run after render
+  // so the code below runs first
+  // effects do AJAX calls, modify ambient state, integrate with other libraries and many other things
+  // Basically it's a way to delay work until after render happens and to deal with asynchronous side effects
+  useEffect(() => {
+    // pet.breeds("dog").then(console.log, console.error);
+    // if there is stuff there then clear it out
+    setBreeds([]);
+    setBreed("");
+
+    // .breed is a function that is returned from the API and you pass is the animal from the dropdown
+    // that retuens back an object with breed in it
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      // we have a list of objects that we want to transform into a list of strings and we do this is .map
+      const breedStrings = apiBreeds.map(({ name }) => name); //.map((breedObj) => breedObj.name)
+      setBreeds(breedStrings);
+    }, console.error); //error => console.error(error)
+  }, [animal, setBreed, setBreeds]); // if any one of these things change then do the AJAX call post render if not then don't
+  // if you want it to only run once the pass an empty array
 
   return (
     //   you can't use class because class is a reserved word in JS so you have to use className
     <div className="search-params">
-      <form action="">
+      <form
+        onSubmit={e => {
+          e.preventDefault(); //don't want to actually submit an HTML form
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           location
           <input
@@ -60,41 +83,11 @@ const SearchParams = () => {
             onChange={e => setLocation(e.target.value)}
           />
         </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={e => updateAnimal(e.target.value)}
-            onBlur={e => updateAnimal(e.target.value)}
-          >
-            <option />
-            {ANIMALS.map(animal => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Breed
-          <select
-            disabled={!breeds.length}
-            id="breed"
-            value={breed}
-            onChange={e => updateBreed(e.target.value)}
-            onBlur={e => updateBreed(e.target.value)}
-          >
-            <option />
-            {breeds.map(breed => (
-              <option key={breed} value={breed}>
-                {breed}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
