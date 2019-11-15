6d18af5d80fd258123b7422b35ac90deebea49d0
Aaron Thompson <aaron.thompson@feedagency.com>
2019-11-15 13:58:11 +0000

    add error boundary js

---
+++ level-7/src/ErrorBoundary.js
@@ -0,0 +1,6 @@
+// mostyl code from the docs readtjs.org/docs/erorboundaries.html
+
+import React, { Component } from "react";
+import { Link } from "@reach/router";
+
+class ErrorBoundary extends Component {}
