45ea9cf5d6043da8e85297e91361fe4acc58f9ce
Aaron Thompson <aaron.thompson@feedagency.com>
2019-11-18 13:58:59 +0000

    end level 7

--- level-7/src/ErrorBoundary.js
+++ level-7/src/ErrorBoundary.js
@@ -6,56 +6,55 @@
 // ALSO NOTE - A COMPONENT CAN ONLUY CATCH ERRORS IN IT'S CHILDREN IT CANNOT CATCH IT'S OWN ERRORS - as such this will be a wrapper for details.js
 // This error boundary since above a component and if there is an error it doesn something about it but if not it will just pass through
 
-import React, {
-    Component
-} from "react";
-import {
-    Link
-} from "@reach/router";
+import React, { Component } from "react";
+import { Link, Redirect } from "@reach/router";
 
 class ErrorBoundary extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            hasError: false
-        };
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, redirect: false };
+  }
+  // there is no equivalent for the below two functions with hooks so you have to use class components
+  static getDerivedStateFromError() {
+    return {
+      hasError: true
+    };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary caught an error", error, info);
+  }
+  // this function will be called if props change or state changes
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 5000);
     }
-    static getDerivedStateFromError() {
-        return {
-            hasError: true
-        };
+  }
+
+  render() {
+    if (this.state.redirect) {
+      // the redirect component comes from reach router
+      return <Redirect to="/" />;
     }
 
-    componentDidCatch(error, info) {
-        console.error("ErrorBoundary caught an error", error, info);
+    if (this.state.hasError) {
+      return (
+        <h1>
+          There was an error with this listing. <Link to="/">Click here</Link>{" "}
+          to back to the home page or wait five seconds
+        </h1>
+      );
     }
 
-    componentWillUpdate() {
-        if (this.state.hasError) {
-            setTimeout(() => this.setState({
-                redirect: true
-            }), 5000);
-        }
-    }
-
-    render() {
-        if (this.state.hasError) {
-            return ( <
-                p > There was an error with this listing. < /p> <
-                Link to = "/" > Click here < /Link> <
-                p > to back to the home page or wait five seconds. < /p>
-            )
-        }
-
-        return this.props.children;
-    }
+    return this.props.children;
+  }
 }
 
 export default ErrorBoundary;
 
 // now  anything that is a child component will have errors caught here
 // this is almost like a try catch block
-// A static method is one that can be called on the constructor 
+// A static method is one that can be called on the constructor
 // here we are calling ErrorBoundary.getDerivedStateFromError(error)
 // if you want to call an error logging service, componentDidCatch would be an amazing place to do this
 // Azure monintor, Sentry and TrackJS
